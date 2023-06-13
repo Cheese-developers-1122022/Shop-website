0cfa134e5c779d1dc4c037d029160eb61024f37b
D4C-Funny-valentine <kyawn5194@gamil.com>
2023-06-14 00:06:55 +0630

    update data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Path from './components/routes/Path'
+import React from "react";
+import Path from "./components/routes/Path";
+import { shopCardData } from "./exportData/exportData";
 
 const App = () => {
+  console.log(shopCardData);
   return (
     <div>
-      <Path/>
+      <Path />
+      {shopCardData.shopItems?.map((item) => (
+        <div className="">
+        <h4>{item.name}</h4>
+        <img src={item.image} alt="" />
+        </div>
+        
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/exportData/exportData.js
@@ -0,0 +1,304 @@
+export const shopCardData = {
+  shopItems: [
+    {
+      id: 1,
+      name: "Adult Quantity Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/2-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/2-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 2,
+      name: "All-Over-Print Hoodie",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/4-12-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/4-13.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 3,
+      name: "AOP Cut & Sew Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-4-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-5.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 4,
+      name: "Fine Jersey Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/17-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/17-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Kids,T-shirt",
+      tags: "kids,t-shirt",
+      priceUp: 31,
+      priceTo: 0,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "Hot",
+    },
+    {
+      id: 5,
+      name: "Fit Round-neck T-shirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-8-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-9.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 6,
+      name: "Hooded Sweatshirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/10-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/10-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Sweater",
+      tags: "t-shirt,woman",
+      priceUp: 28,
+      priceTo: 0,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "Hot",
+    },
+    {
+      id: 7,
+      name: "Kids Hoodie",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/16-2-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/16-3.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie,Kids",
+      tags: "kids,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 8,
+      name: "Lightweight Fashion Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/1-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: null,
+      priceUp: 36,
+      priceTo: 0,
+      discountPrice: 32,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 9,
+      name: "Midweight Cotton Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/10-4-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/10-5.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Sweater,T-shirt",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 10,
+      name: "Premium Crewneck Sweatshirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/3-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/3-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "T-shirt",
+      tags: "designer,t-shirt",
+      priceUp: 39,
+      priceTo: 0,
+      discountPrice: 29,
+      onsale: "-26%",
+      brand: "New",
+    },
+    {
+      id: 11,
+      name: "Premium Pullover Hoodie",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/5-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/5-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 12,
+      name: "Pullover Hoodie Sweatshirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/3-4-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/3-5.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 13,
+      name: "Relaxed Fit Sweatshirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/2-4-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/2-5.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Hoodie",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 14,
+      name: "Softstyle Tank Top",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/8-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/8-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Tanktop",
+      tags: "designer,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+    {
+      id: 15,
+      name: "Toodler T-shirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/17-2-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/17-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Kids,T-shirt",
+      tags: "kids,t-shirt",
+      priceUp: 26,
+      priceTo: 0,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "Hot",
+    },
+    {
+      id: 16,
+      name: "Youth Short Sleeve Tee",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/14-330x440.jpg",
+      backdrop_image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/14-1.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Kids,T-shirt",
+      tags: "kids,t-shirt",
+      priceUp: 26,
+      priceTo: 29,
+      discountPrice: 0,
+      onsale: "Sale",
+      brand: "New",
+    },
+  ],
+  customItems: [
+    {
+      id: 1,
+      name: "Zone Sweatshirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/8-330x440.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Product Designer",
+      tags: "designer",
+      priceUp: 19.95,
+      priceTo: 159.95,
+    },
+    {
+      id: 2,
+      name: "Zoo Men's T-shirt",
+      image:
+        "https://teespace.harutheme.com/studio/wp-content/uploads/sites/3/2022/10/10-2-330x440.jpg",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt odio quam, aliquam amet exercitationem quis! Officiis cumque pariatur quidem rerum",
+      category: "Product Designer",
+      tags: "designer",
+      priceUp: 14.95,
+      priceTo: 119.95,
+    },
+  ],
+  shopItemsTotal: 18,
+  customItemsTotal: 2,
+};
