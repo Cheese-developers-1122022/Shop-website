b4470ca9e8cefcdc5343f809a46a0661f9c6beb9
SutPi0154 <sutpi070@gmail.com>
2023-06-21 04:12:08 +0630

    Navbar done and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Path from "./components/routes/Path";
 import { shopCardData } from "./exportData/exportData";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   console.log(shopCardData);
   return (
     <div>
+      <Navbar />
       <Path />
-      {shopCardData.shopItems?.map((item) => (
-        <div className="">
-        <h4>{item.name}</h4>
-        <img src={item.image} alt="" />
-        </div>
-        
-      ))}
+      <div className=" flex justify-center items-center flex-wrap">
+        {shopCardData.shopItems?.map((item) => (
+          <div key={item?.id}>
+            <h4>{item.name}</h4>
+            <img src={item.image} alt="" />
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { SpotlightProvider } from "@mantine/spotlight";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
